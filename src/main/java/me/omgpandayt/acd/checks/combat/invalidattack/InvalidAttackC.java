daf8f66454a9935ded2bfa1f8fc1acd949850589
OMGPandaYT <67205635+OMGPandaYT@users.noreply.github.com>
2021-08-08 15:41:29 -0600

    b1.3.8
    - Added InvalidAttackC
    - Removed block_in_way from InvalidAttackA (is now in C check)
    - Fixed JesusE falsing on carpet

---
+++ src/main/java/me/omgpandayt/acd/checks/combat/invalidattack/InvalidAttackC.java
@@ -0,0 +1,37 @@
+package me.omgpandayt.acd.checks.combat.invalidattack;
+
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+import me.omgpandayt.acd.checks.Check;
+import me.omgpandayt.acd.violation.Violations;
+
+public class InvalidAttackC extends Check {
+	
+	public InvalidAttackC() {
+		super("InvalidAttackC", false);
+	}
+	
+	@Override
+	public void onDamage(EntityDamageByEntityEvent e) {
+		
+		
+		Player p = (Player) e.getDamager();
+		
+		Block targetBlock = p.getTargetBlockExact(5);
+		if(targetBlock == null || !targetBlock.getType().isSolid())return;
+		
+		double targetDis = targetBlock.getLocation().distance(p.getLocation());
+		double entityDis = e.getEntity().getLocation().distance(p.getLocation());
+		
+		if(targetDis + config.getDouble(path + "block-distance-increase") < entityDis) {
+		
+			flag(p, "InvalidAttack (C)", "(VL" + (Violations.getViolations(this, p)+1) + ") (DIST " + ((Math.floor(((entityDis - (targetDis + config.getDouble(path + "block-distance-increase")))) * 100))/100) + ")");
+			cancelDamage(e);
+		
+		}
+		
+	}
+	
+}
